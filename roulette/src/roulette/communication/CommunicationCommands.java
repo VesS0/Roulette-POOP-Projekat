97976aa056b8afee16b90797b6c03ed9f2f489fa
Nikola Miljkovic <milja1337@gmail.com>
2015-05-17 14:12:30 +0200

    added missing communication commands for bets

--- roulette/src/roulette/communication/CommunicationCommands.java
+++ roulette/src/roulette/communication/CommunicationCommands.java
@@ -12,10 +12,22 @@ package roulette.communication;
 public class CommunicationCommands 
 {
     private CommunicationCommands() { }
+    
+    // main client server messages
     public static final String JOIN_MESSAGE = "JOIN";
     public static final String WELCOME_MESSAGE = "WELCOME";
     public static final String QUIT_MESSAGE = "QUIT";
     public static final String QUIT_RESPONSE = "BYE";
     public static final String STATE_REQUEST = "STATE";
-    // ... dodati opise drugih komandi
+    
+    // other messages 
+    public static final String MANQUE = "MANQUE";
+    public static final String PASSE = "PASSE";
+    public static final String ROUGE = "ROUGE";
+    public static final String NOIR = "NOIR";
+    public static final String PAIR = "PAIR";
+    public static final String IMPAIR = "PAIR";
+    public static final String SINGLE = "SINGLE_";
+    public static final String COLUMN = "COLUMN_";
+    public static final String ROW = "ROW_";
 }
